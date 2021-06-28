96c5d7f000db1193a54b1fafd11bf1a06f91ed73
Pupkin <rasraziel@gmail.com>
2021-06-28 22:08:57 +0200

    Heroku deploy

--- app/components/sidesEntry/sidesEntry.service.js
+++ app/components/sidesEntry/sidesEntry.service.js
@@ -1,6 +1,9 @@
 app.service("sidesHttpSvc", function ($http) {
 
+    const url = 'https://triangle-back.herokuapp.com';
+    // const url = 'http://127.0.0.1:8020';
+
     this.sendSides = (sideData) => {
-        return $http.post("http://127.0.0.1:8020/sides", sideData).then(res => { return res });
+        return $http.post(`${url}/sides`, sideData).then(res => { return res });
     }
 });
