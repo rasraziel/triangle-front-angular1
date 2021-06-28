ced3078154bb8c56392370dd93f6793269b84322
Pupkin <rasraziel@gmail.com>
2021-06-29 00:20:11 +0200

    Heroku deploy

--- app/components/pointsEntry/pointsEntry.service.js
+++ app/components/pointsEntry/pointsEntry.service.js
@@ -1,7 +1,7 @@
 app.service("pointsHttpSvc", function ($http) {
 
-    const url = 'https://triangle-back.herokuapp.com';
-    // const url = 'http://127.0.0.1:8080';
+    // const url = 'https://triangle-back.herokuapp.com';
+    const url = 'http://127.0.0.1:8080';
 
     this.sendPoints = (pointData) => {
         return $http.post(`${url}/points`, pointData).then(res => { return res });
