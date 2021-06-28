0f042ac0bafcb710d5475f3f0c1b2d21ade22834
Pupkin <rasraziel@gmail.com>
2021-06-29 00:17:11 +0200

    Heroku deploy

--- app/components/form/form.controller.js
+++ app/components/form/form.controller.js
@@ -47,11 +47,13 @@ function Main($rootScope) {
         var layer = new Konva.Layer();
         var poly = new Konva.Line({
             points: [$rootScope.axis.a.x, 300 - $rootScope.axis.a.y, $rootScope.axis.b.x, 300 - $rootScope.axis.b.y, $rootScope.axis.c.x, 300 - $rootScope.axis.c.y],
-            fill: 'red',
             stroke: 'black',
             strokeWidth: 1,
             closed: true,
-            draggable: true
+            draggable: true,
+            fillLinearGradientStartPoint: { x: $rootScope.axis.a.x, y: 300 - $rootScope.axis.a.y },
+            fillLinearGradientEndPoint: { x: $rootScope.axis.c.x, y: 300 - $rootScope.axis.c.y },
+            fillLinearGradientColorStops: [0, "#" + Math.floor(Math.random() * 16777215).toString(16), 1, "#" + Math.floor(Math.random() * 16777215).toString(16)]
         });
         // add the shape to the layer
         layer.add(poly);
--- app/components/sidesEntry/sidesEntry.service.js
+++ app/components/sidesEntry/sidesEntry.service.js
@@ -1,7 +1,7 @@
 app.service("sidesHttpSvc", function ($http) {
 
-    // const url = 'https://triangle-back.herokuapp.com';
-    const url = 'http://127.0.0.1:8080';
+    const url = 'https://triangle-back.herokuapp.com';
+    // const url = 'http://127.0.0.1:8080';
 
     this.sendSides = (sideData) => {
         return $http.post(`${url}/sides`, sideData).then(res => { return res });
